0bf3385dd953cc05df186f094b89eccb61756b7e
jjangsky <dbcks4646@gmail.com>
2025-08-10 23:56:58 +0900

    feat: base settings

--- initProject/screens/MainScreen.js
+++ initProject/screens/MainScreen.js
@@ -1,11 +1,19 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const MainScreen = () => {
     return (
         <SafeAreaView>
-            <Text>MainScreen</Text>
+            <StatusBar barStyle="default" />
+            <Text>TODO APP</Text>
+            <View>
+                <Text>할 일</Text>
+            </View>
+            <View />
+            <View>
+                <Text>완료된 일</Text>
+            </View>
         </SafeAreaView>
     );
 };
