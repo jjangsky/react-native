9a63bc0b21ed1afc5f4d497e1591386b43fc8768
jjangsky <dbcks4646@gmail.com>
2025-08-10 23:50:21 +0900

    refactor: expo 53 폴더 라우팅구조로 인한 프로젝트 구조 변경

---
+++ initProject/app/index.tsx
@@ -0,0 +1,5 @@
+import MainScreen from '../screens/MainScreen';
+
+export default function HomeScreen() {
+    return <MainScreen />;
+}
---
+++ initProject/screens/MainScreen.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const MainScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>MainScreen</Text>
+        </SafeAreaView>
+    );
+};
+
+export default MainScreen;
