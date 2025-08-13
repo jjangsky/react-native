fc47de099801c252b6b550b8ab043920aeae4296
jjangsky <dbcks4646@gmail.com>
2025-08-13 23:50:27 +0900

    로그인 화면 생성

--- initProject/screens/LoginScreen.js
+++ initProject/screens/LoginScreen.js
@@ -1,14 +1,88 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import ListIcon from '../assets/svg/list.svg';
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <View>
-            <Text>LoginScreen</Text>
+        <View style={styles.container}>
+            <ListIcon />
+            <View style={styles.inputcontainer}>
+                <TextInput
+                    placeholder="이메일"
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="비밀번호"
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                />
+            </View>
+            <View styles={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>로그인</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={(styles.button, styles.buttonOutline)}>
+                    <Text style={styles.buttonOutline}>회원 가입</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    inputContainer: {
+        width: '80%',
+        marginTop: 15,
+    },
+    input: {
+        backgroundColor: 'white',
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    buttonContainer: {
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30,
+    },
+    button: {
+        backgroundColor: 'black',
+        width: '100%',
+        padding: 15,
+        borderRadius: 10,
+        alignItems: 'center',
+    },
+    buttonOutline: {
+        backgroundColor: 'white',
+        marginTop: 5,
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: '500',
+        fontSize: 16,
+    },
+    buttonOutlineText: {
+        color: 'black',
+        fontWeight: '500',
+        fontSize: 16,
+    },
+});
