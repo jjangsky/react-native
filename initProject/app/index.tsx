8a8718268c7d198f17d65645a2ee371bc1352bb3
jjangsky <dbcks4646@gmail.com>
2025-08-13 23:26:33 +0900

    navigation 생성

--- initProject/app/index.tsx
+++ initProject/app/index.tsx
@@ -1,11 +1,29 @@
 import { Provider } from 'react-redux';
 import MainScreen from '../screens/MainScreen';
+import LoginScreen from '../screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
 import { store } from '../redux/store';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function HomeScreen() {
-   return (
-      <Provider store={store}>
-         <MainScreen />
-      </Provider>
-   );
+    return (
+        <Provider store={store}>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        options={{ headerShown: false }}
+                        name="Main"
+                        component={MainScreen}
+                    />
+                    <Stack.Screen
+                        options={{ headerShown: false }}
+                        name="Login"
+                        component={LoginScreen}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </Provider>
+    );
 }
