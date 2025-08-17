d5d9edac396b231e853b6ce57e88e6c2e16575d5
jjangsky <dbcks4646@gmail.com>
2025-08-17 19:15:34 +0900

    스토리 상세 컴포넌트 생성

--- ReactNativeInstaApp/src/screens/Status.js
+++ ReactNativeInstaApp/src/screens/Status.js
@@ -1,11 +1,83 @@
 import { View, Text } from 'react-native';
+import { StatusBar } from 'react-native/types_generated/index';
 import React from 'react';
 
-const Status = () => {
+const Status = ({ route, navigation }) => {
+  const { name, image } = route.params;
+
   return (
-    <View>
-      <Text>Status</Text>
-    </View>
+    // navigation으로 전달받은 정볼르 route로 전달 받아서 사용함
+    <SafeAreaView
+      style={{
+        backgroundColor: 'black',
+        height: '100%',
+        justifyContent: 'center',
+      }}
+    >
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <View
+        style={{
+          height: 3,
+          width: '95%',
+          boderWidth: 1,
+          backgroundColor: 'gray',
+          position: 'absolute',
+          top: 18,
+        }}
+      ></View>
+      <View
+        style={{
+          padding: 15,
+          flexDirection: 'row',
+          alignItems: 'center',
+          position: 'absolute',
+          top: 12,
+          left: 0,
+          width: '90%',
+        }}
+      >
+        <View
+          style={{
+            width: 30,
+            height: 30,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Image
+            source={image}
+            style={{
+              boederRadius: 100,
+              backgroundColor: 'orange',
+              width: '92%',
+              height: '92%',
+              resizeMode: 'cover',
+            }}
+          />
+        </View>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            width: '100%',
+          }}
+        >
+          <Text style={{ color: 'white', fontSize: 15, paddingLeft: 10 }}>
+            {name}
+          </Text>
+          <TouchableOpacity onpress={() => navigation.goBack()}>
+            <Ionic
+              name="close"
+              style={{ color: 'white', fontSize: 15, opacity: 0.6 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Image
+        source={image}
+        style={{ position: 'absolute', width: '100%', height: 600 }}
+      />
+    </SafeAreaView>
   );
 };
 
